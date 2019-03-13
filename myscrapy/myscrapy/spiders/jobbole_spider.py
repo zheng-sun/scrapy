d6a1c8567da31288c46d43b66d5f2ad46ace0db5
孙正 <2684176932@qq.com>
2019-03-13 18:26:36 +0800

    处理面向对象详情页面数据

--- myscrapy/myscrapy/spiders/jobbole_spider.py
+++ myscrapy/myscrapy/spiders/jobbole_spider.py
@@ -16,8 +16,50 @@ class JobboleSpider(scrapy.Spider):
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
     def parse_data(self, response):
-        yield {
-            'title': response.css('h1.p-tit-single::text').extract_first(),
-            'release_time': response.css('p.p-meta span::text').extract_first(),
-            #'city':res
-        }
+        content_list = self.contentHandle(response)
+
+        if content_list is not None:
+            content_list['title'] = response.css('h1.p-tit-single::text').extract_first()
+            content_list['release_time'] = response.css('p.p-meta span::text').extract_first()
+            content_list['city'] = response.css('p.p-meta span')[1].css('a::text').extract_first()
+            # yield {
+            #     'title': response.css('h1.p-tit-single::text').extract_first(),
+            #     'release_time': response.css('p.p-meta span::text').extract_first(),
+            #     'city': response.css('p.p-meta span')[1].css('a::text').extract_first(),
+            #     'content': content_list
+            # }
+        yield content_list
+
+    def contentHandle(self, response):
+        content = response.css('.p-entry p').extract_first()
+        if content is not None:
+            content_list = content.split('<br>\n')
+
+        if len(content_list) <= 1:
+            return None
+
+        details = {}
+        data_key = ['birth', # 出生日期
+                'height', #身高
+                'weight', #体重
+                'Education', #学历
+                'Location', #所在城市
+                'Household', #户籍
+                'Native_place', #籍贯
+                'Occupation',  #职业
+                'income',   #收入
+                'Hobby',  #兴趣爱好
+                'Long-distance-love',  #是否接受异地恋
+                'marry-year', #打算几年内结婚
+                'word',   #一句话脱颖而出
+                'about_me', #自我介绍
+                ]
+
+        count = 0
+        for data in content_list:
+            if count > 13:
+                break
+            data_rows = data.split('：')
+            details[data_key[count]] = data_rows[1]
+            count += 1
+        return details
